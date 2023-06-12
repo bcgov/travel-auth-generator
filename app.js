61e51fc8a8cca70389ef2bff567cfbe44f68d20d
jTendeck <justin.tendeck@gov.bc.ca>
2023-06-12 15:10:39 -0700

    Add basic axios functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 var bodyParser = require("body-parser");
 var path = require('path');
+const axios = require('axios');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,9 +14,20 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-app.post('/sample/put/data', function(req, res) {
+app.post('/submit-traveler-data', function(req, res) {
+  
+  const { lastName, firstName } = req.body;
+
+  axios.get('https://httpbin.org/anything'
+  ).then (res => {
+    console.log(res);
+  }).catch(err => {
+    console.log(err);
+  })
+
   console.log('receiving data ...');
   console.log('body is ',req.body);
+
   res.send(req.body);
 });
 
